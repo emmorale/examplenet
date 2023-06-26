5ef71558422952d937ba5ebf9184263d147856d9
Emmanuel Morales <emorales@interware.com.mx>
2023-06-26 13:45:19 -0500

    trying different port

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace YourAppName
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:8080");
                 });
     }
 }

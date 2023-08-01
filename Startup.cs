e5e27dbd1f4a1e41979de44dd70653c601a71ea7
Emmanuel Morales <emorales@interware.com.mx>
2023-08-01 10:29:48 -0500

    adding probes

--- Startup.cs
+++ Startup.cs
@@ -42,6 +42,35 @@ namespace YourAppName
                     await context.Response.BodyWriter.WriteAsync(responseBytes);
 
                 });
+
+                // Startup Probe - Returns 200 status code to indicate that the application has completed its initialization
+                // For simplicity, we will assume the application is ready immediately after startup
+                endpoints.MapGet("/health/startup", async context =>
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Startup Probe: OK");
+                });
+
+                // Liveness Probe - Returns 200 status code when the application is alive
+                endpoints.MapGet("/health/liveness", async context =>
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Liveness Probe: OK");
+                });
+
+                // Readiness Probe - Returns 200 status code when the application is ready to serve requests
+                endpoints.MapGet("/health/readiness", async context =>
+                {
+                    // Add your own readiness check logic here.
+                    // For example, if your application depends on a database or other services,
+                    // check if the required dependencies are available and ready to serve requests.
+
+                    // If your application is ready, return 200 status code.
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Readiness Probe: OK");
+                });
+
+
             });
         }
     }
